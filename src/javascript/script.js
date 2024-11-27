c1f566476ab85e54ed09726412c685cc9127c0fe
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2024-11-27 15:36:53 +0100

    Change some tailwind propreities and also i added a some style in css native for media queries to convert the website to more responsivable design and modifing in fitching card by adding the country flag of any player, position; and finnaly i added a simple footer

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -6,13 +6,13 @@ function displayplayers(data){
     const cardPlayer = document.getElementById("dis-ply")
     cardPlayer.innerHTML = data.map((player) =>{
         return `<div class="flex flex-wrap gap-2">
-                     <div class="flex flex-wrap border border-black gap-2 bg-[#E6E9AF] rounded-t-full w-32">
+                     <div class="flex flex-wrap border border-black gap-2 bg-[#E6E9AF] w-24 ">
                        <figure class="w-full flex justify-center">
-                           <img class="rounded-t-full bg-[#E8E8D8] w-full h-32 border-b border-black" src="${player.photo}" alt="${player.name}" title="${player.name}">
+                           <img class="bg-[#E8E8D8] w-full h-24 border-b border-black" src="${player.photo}" alt="${player.name}" title="${player.name}">
                        </figure>
                        <div class="flex flex-col justify-center items-center w-full pb-2">
                            <h1 class="text-lg font-medium ">${player.name}</h1>
-                           <h1 class="text-sm">${player.rating}</h1>
+                           <h1 class="text-sm flex items-center"><img class="bg-[#E8E8D8] w-3 h-2 border-b border-black" src="${player.flag}" alt="${player.nationality}" title="${player.nationality}"> | ${player.position} | ${player.rating}</h1>
                        </div>
                      </div> 
                    </div>`
